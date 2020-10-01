b44cc4e422b235a855577d892a270b6ca63906a9
Jonatas Souza <jonatasfelipe2@hotmail.com>
2020-10-01 10:02:31 -0300

    Finished challenge 4

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,47 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    const { transactions } = this;
+
+    return transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.transactions.reduce(
+      (acc, curr) => {
+        switch (curr.type) {
+          case 'income':
+            return {
+              ...acc,
+              income: acc.income + curr.value,
+              total: acc.total + curr.value,
+            };
+          case 'outcome':
+            return {
+              ...acc,
+              outcome: acc.outcome + curr.value,
+              total: acc.total - curr.value,
+            };
+          default:
+            return acc;
+        }
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
